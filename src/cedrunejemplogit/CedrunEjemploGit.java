ab71223f4f3055c7fe0f7c6bd593bb722a603383
damt105 <damt105@36-06.aglinformatica.es>
2022-05-05 19:36:05 +0200

    Version 2

--- src/cedrunejemplogit/CedrunEjemploGit.java
+++ src/cedrunejemplogit/CedrunEjemploGit.java
@@ -1,22 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cedrunejemplogit;
 
-/**
- *
- * @author damt105
- */
 public class CedrunEjemploGit {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hola, soy Héctor");
-        System.out.println("Versión 1");
+        System.out.println("Versión 2");
     }
     
 }
